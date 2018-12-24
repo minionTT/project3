2e764f5205d11ccf29e7000e3a9ef54e51e34bdd
Tanya <tanya1.shih@gmail.com>
2018-12-24 23:37:05 +0800

    version 2

--- project3.cpp
+++ project3.cpp
@@ -6,14 +6,22 @@ class Student{
             int firstI,firstJ,maxI,maxJ;
             int M = 0;
             bool findFirst = false,findMax = false;
+            int num = 0;
             for(i=0; i<5; i++){
                 for(j=0; j<6; j++){
                     if(color[i][j] == inputColor){
+                        num++;
                         if(Record[i][j] > M){
                             maxI = i;
                             maxJ = j;
                             M = Record[i][j];
                             findMax = true;
+                        }else if(Record[i][j] == M){
+                            if((i+j)%3==0){
+                                maxI = i;
+                                maxJ = j;
+                                M = Record[i][j];
+                            }
                         }
                     }
                     if(color[i][j] == White){
@@ -21,13 +29,29 @@ class Student{
                             firstI = i;
                             firstJ = j;
                             findFirst = true;
+                        }else{
+                            if((i+j)%3==1){
+                                firstI = i;
+                                firstJ = j;
+                            }
                         }
                     }
                 }
             }
-            if(findMax){
+            if(M == 3 || M == 2){
                 x = maxI;
                 y = maxJ;
+            }else if(findMax){
+                if(!findFirst){
+                    x = maxI;
+                    y = maxJ;
+                }else if(num%2==0){
+                    x = maxI;
+                    y = maxJ;
+                }else{
+                    x = firstI;
+                    y = firstJ;
+                }   
             }else{
                 x = firstI;
                 y = firstJ;
